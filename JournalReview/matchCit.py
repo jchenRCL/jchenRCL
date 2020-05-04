6a41707aadc551bfc0a2763ad5979f80eaa1d66c
jchenRCL <63127123+jchenRCL@users.noreply.github.com>
2020-05-04 11:44:20 -0400

    Update matchCit.py

--- JournalReview/matchCit.py
+++ JournalReview/matchCit.py
@@ -1,5 +1,4 @@
 # match parent data's column C (abbreviated title) with citation data's CR column
-
 def MatchCitation(data, parent):
     # add a JCR Abbreviated Title column to data
     data['JCR Abbreviated Title']=data['CR']
@@ -8,3 +7,5 @@ def MatchCitation(data, parent):
     result=parent.join(data.set_index('JCR Abbreviated Title'), on='JCR Abbreviated Title')
     
     return result # this is the matched file, export
+
+
