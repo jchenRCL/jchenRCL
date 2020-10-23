f29699119184e89054028c5ccfc746bcedddf3ef
jchenRCL <63127123+jchenRCL@users.noreply.github.com>
2020-10-23 08:10:59 -0400

    update the script

--- Times/get_uni_citations_by_subjects.py
+++ Times/get_uni_citations_by_subjects.py
@@ -2,12 +2,12 @@ university = []
 metric_type = []
 the_code =[]
 value = []
-MY_API = "XXXXXXXXXXXXXXXXX"
+MY_API = "XXXXXXXXXXXXXXX"
 
 url = "https://api.elsevier.com/analytics/scival/institution/metrics?metricTypes=CitationCount&institutionIds={}&yearRange=5yrs&subjectAreaFilterURI=Class%2FTHE%2FCode%2F{}&includeSelfCitations=false&byYear=false&includedDocs=ArticlesReviewsConferencePapersBooksAndBookChapters&journalImpactType=CiteScore&showAsFieldWeighted=true&apiKey={}"
-
-for uid in join_result['University Id'][:50]:
-    for the_id in the_asjc['THE_subject_id']:
+for the_id in [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]:
+    for uid in join_result['University Id'][:2]:
+#    for uid in join_result['University Id'][:50]:    
         resp = requests.get(url.format(uid, the_id, MY_API), headers={'Accept':'application/json'})
         if not 200:
             print("An error has occured. [Status code", resp, "]")
@@ -37,4 +37,4 @@ for uid in join_result['University Id'][:50]:
             
             
 df = pd.DataFrame({'Uni Name': university, 'Metrics': metric_type, 'THE code': the_code, 'Metric Value': value})
-df.to_csv("the_2021_us_uni_citation_count.csv")
+df.to_csv("the_file_name.csv")
